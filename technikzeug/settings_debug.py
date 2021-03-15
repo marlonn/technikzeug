65372a078714769c639d17a1b3024b12b354ea71
Correct Name <marlon.neumann@mailbox.org>
2021-03-15 17:33:19 +0000

    configured project to run in production mode with nginx and a s ystemwide-installed uwsgi

--- technikzeug/settings_debug.py
+++ technikzeug/settings_debug.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(^6iuw*i4b@7**(h+)z@zp%$^lo3!s2y+gh%a00u=&^q)6hkb!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -160,4 +160,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
